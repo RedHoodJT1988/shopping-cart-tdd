11a4b37cb9af0d7e7d6c001e710c1e30fc6aacb9
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2020-02-28 10:39:15 -0600

    third test passing

--- src/test/java/shopping/cart/cart/CartTest.java
+++ src/test/java/shopping/cart/cart/CartTest.java
@@ -20,7 +20,7 @@ public class CartTest {
     }
 
     // Given that I have an empty cart, when I add an item, then I expect to getTotalPrice() reflect the sum
-    // of all the Items in my cart, times the quantites of each item
+    // of all the Items in my cart, times the quantities of each item
     @Test
     public void AddItemsToCart() {
         Cart cart = new Cart();
@@ -34,4 +34,26 @@ public class CartTest {
         cart.AddItem(item2);
         assertEquals(750.99, cart.getTotalPrice());
     }
+
+    // Given I have an empty cart, when I add more than one of an item, then I expect itemQuantities() to show
+    // the number of items I have added.
+    @Test
+    public void DisplayItemiziedList() {
+        Cart cart = new Cart();
+        assertEquals(cart.itemizedList(), Collections.EMPTY_LIST);
+
+        Item item1 = new Item("Gucci Blazer", 250.00);
+        Item item2 = new Item("Louis Vatton Sunglasses", 150.00);
+        cart.AddItem(item1);
+        cart.AddItem(item2);
+        assertEquals(new ArrayList<String>(Arrays.asList("Gucci Blazer - $250.0", "Louis Vatton Sunglasses - $150.0")), cart.itemizedList());
+    }
+    // Given I have an empty cart, when I add items, then I expect itemizedList() reflect the items I have added
+    // along with their price and quantity.
+
+    // Given I have an empty cart, when I add more than one of an item, then I expect getTotalPrice()
+    // to reflect both the item price and quantity.
+
+    // Given I have a cart with items that are not on sale, when I add items that are on sale, I expect
+    // onSaleItems() to include only the items on sale.
 }
